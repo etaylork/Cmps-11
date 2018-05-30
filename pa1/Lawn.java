79f750241e9680b9bb2db1de50e1d68bf72808f0
Elijah Taylor-Kuni <elijahtk@qualitestgroup.com>
2018-05-30 16:15:41 -0700

    cleaned up code

--- pa1/Lawn.java
+++ pa1/Lawn.java
@@ -1,42 +1,39 @@
 import java.util.*;
 
-class Lawn{
+class Lawn {
 
-	public static void main(String args[]){
+    public static void main(String args[]) {
 
-		Scanner sc = new Scanner(System.in);
-		double l1,w1,l2,w2; 
-		double lawn, rate; 
-		int s,m,h; 
+        Scanner sc = new Scanner(System.in);
+        double l1, w1, l2, w2;
+        double lawn, rate;
+        int s, m, h;
 
-		System.out.print("Enter the length and width of the lot, in feet: ");
-         l1 = sc.nextDouble();
-         w1 = sc.nextDouble();
+        System.out.print("Enter the length and width of the lot, in feet: ");
+        l1 = sc.nextDouble();
+        w1 = sc.nextDouble();
         System.out.print("Enter the length and width of the house, in feet: ");
-         l2 = sc.nextDouble();
-         w2 = sc.nextDouble();
+        l2 = sc.nextDouble();
+        w2 = sc.nextDouble();
 
-        //find the area of the lawn 
-        lawn = (l1*w1) - (l2*w2); 
-        System.out.println("The lawn is " + lawn +" square feet.");
+        // find the area of the lawn
+        lawn = (l1 * w1) - (l2 * w2);
+        System.out.println("The lawn is " + lawn + " square feet.");
 
-        //Mowing speed 
+        // Mowing speed
         System.out.print("Enter the mowing rate, in square feet per second: ");
         rate = sc.nextDouble();
 
-        rate  = lawn/rate; 
+        rate = lawn / rate;
         s = (int) Math.round(rate);
-        m = s/60;
-        s = s%60;
-        h = m/60;
-        m = m%60;
+        m = s / 60;
+        s = s % 60;
+        h = m / 60;
+        m = m % 60;
 
-        System.out.println("the mowing time is " + h + (h== 1 ? " hour " : " hours ") + m + (m == 1 ? " minute " : " minutes ")
-        + s + (s == 1 ? " second. " : " seconds. ")  ); 
+        System.out.println("the mowing time is " + h + (h == 1 ? " hour " : " hours ") + m
+                + (m == 1 ? " minute " : " minutes ") + s + (s == 1 ? " second. " : " seconds. "));
 
-
-
-
-   	}
+    }
 
 }
--- pa3/GCD.java
+++ pa3/GCD.java
@@ -1,49 +1,45 @@
 import java.util.*;
 
-class GCD{
+class GCD {
 
-  // method that finds the Greatest Common Demoniator of 2 integers 
-  static int findGCD(int x, int z){
-  	int r = x%z; // get the remainder 
-  	int result = 0;
-  	while(r != 0){
-  		x = z;
-  	    z = r;
-  	    result = r;
-  	    r = x%z;
+	// method that finds the Greatest Common Demoniator of 2 integers
+	static int findGCD(int x, int z) {
+		int r = x % z; // get the remainder
+		int result = 0;
+		while (r != 0) {
+			x = z;
+			z = r;
+			result = r;
+			r = x % z;
 
-  	}
-  	 return result; 
-  	} 
-  
+		}
+		return result;
+	}
 
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Scanner sc2 = new Scanner(System.in);
 
-  public static void main(String[] args){
-	Scanner sc = new Scanner(System.in);
-	Scanner sc2 = new Scanner(System.in);
+		// Loop until you get a positive integer
+		System.out.print("Enter a positive integer: ");
+		while (!sc.hasNextInt()) {
+			System.out.print("Please enter a positive integer: ");
+			sc.next(); // goes to the next thing you put in the sc object
+		}
+		// second loop until you get a positive integer
+		System.out.print("Enter another positive integer: ");
+		while (!sc2.hasNextInt()) {
+			System.out.print("Please enter a positive integer: ");
+			sc2.next();
+		}
 
-    // Loop until you get a positive integer
-	System.out.print("Enter a positive integer: ");
-	while(!sc.hasNextInt()){
-		System.out.print("Please enter a positive integer: ");
-		sc.next(); // goes to the next thing you put in the sc object 
- 	}
- 	// second loop until you get a positive integer
-    System.out.print("Enter another positive integer: ");
-    while(!sc2.hasNextInt()){
-    	System.out.print("Please enter a positive integer: ");
-    	sc2.next();
-    }
+		int a = sc.nextInt();
+		int b = sc2.nextInt();
 
-    int a = sc.nextInt();
-    int b = sc2.nextInt();
+		if (a > b)
+			System.out.println("The GCD of " + a + " and " + b + " is " + findGCD(a, b));
+		else
+			System.out.println("The GCD of " + a + " and " + b + " is " + findGCD(b, a));
 
-   if(a > b)
-     System.out.println("The GCD of "+ a + " and " + b + " is " + findGCD(a,b));
-   else
-   	  System.out.println("The GCD of "+ a + " and " + b + " is " + findGCD(b,a));
-
-
-    
- }
+	}
 }
